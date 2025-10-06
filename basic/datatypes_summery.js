9daded5d7947c944b577a4344219b4ef3e9c5662
Saikat-93 <saikatsamanta624@gmail.com>
2025-10-06 23:16:50 +0530

    add datatypes summery

---
+++ basic/datatypes_summery.js
@@ -0,0 +1,18 @@
+//stack(primitive) ,heap(non-priimitive)
+let myName="saikat"
+let anotherName=samanta;
+anotherName="hllo";
+console.log(myName);
+console.log(anotherName);
+
+let username={
+    email: "saikat samanta",
+    upi:"user@ybl"
+}
+
+let userone= username
+
+userTwo.email="samantasaikat.gmail.com"
+
+console.log(userTwo.email);
+console.log(userone.email);
